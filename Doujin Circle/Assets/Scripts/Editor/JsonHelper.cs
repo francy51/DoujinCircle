00171aadf1be7522c63a7d1085ca71bf850cd140
Francesco <francesco12382@gmail.com>
2017-09-11 18:35:36 +0800

    Changed Character class and editor so that multiple languages can be added and then saved. Next feature to be added to the editor is the option to delte language or poses. This should have a prompt asking if it is what the user actually wants to do. Once this is done then the character creation tool only needs a bit of styling here and there but this can be done later when others have to use the tool so that there life is easier. Next step would be starting the dialogue tree creation tool.

---
+++ Doujin Circle/Assets/Scripts/Editor/JsonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+public class JsonHelper<T> where T : class
+{
+
+    public static T CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<T>(jsonString);
+    }
+
+    public static void SaveToJson(T obj,string path)
+    {
+        string json = JsonUtility.ToJson(obj);
+        File.WriteAllText(path, json);
+    }
+
+
+
+}
---
+++ Doujin Circle/Assets/Scripts/Misc/GameText.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameText 
+{
+
+    [SerializeField]
+    string languageName;
+    [SerializeField]
+    string description;
+    [SerializeField]
+    AudioClip voiceOver;
+
+    public GameText()
+    {
+        languageName = "";
+        description = "";
+        voiceOver = new AudioClip();
+    }
+
+
+    public string LanguageName
+    {
+        get
+        {
+            return languageName;
+        }
+
+        set
+        {
+            languageName = value;
+        }
+    }
+
+    public string Info
+    {
+        get
+        {
+            return description;
+        }
+
+        set
+        {
+            description = value;
+        }
+    }
+
+    public AudioClip VoiceOver
+    {
+        get
+        {
+            return voiceOver;
+        }
+
+        set
+        {
+            voiceOver = value;
+        }
+    }
+}
